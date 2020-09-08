a466668ea6ce3c97bb59628f9b2482eed6c9752c
choi <younggeunn@naver.com>
2020-09-08 21:38:14 +0900

    소수 구하기

---
+++ src/etc/PrimeNumber.java
@@ -0,0 +1,25 @@
+package etc;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		
+		boolean isPrime;
+		
+		for (int i = 2; i < 1000; i++) {
+			isPrime = true;
+			for (int j = 2; j < i; j++) {
+				if (i % j == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+			
+			if (isPrime) {
+				System.out.println(i);
+			}
+		}
+		
+	}
+
+}
