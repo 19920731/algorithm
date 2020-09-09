18f755d7e9a7e12ef2dde36a68bec10b1e230347
choi <younggeunn@naver.com>
2020-09-09 22:21:04 +0900

    팩토리얼(재귀)

---
+++ src/recursion/Factorial.java
@@ -0,0 +1,21 @@
+package recursion;
+
+public class Factorial {
+
+	public static int factorial(int n) {		
+		if (n > 1) {
+			System.out.print(n + " * ");
+			return n * factorial(n - 1);
+		} else {
+			System.out.print("1 = ");
+			return 1;
+		}
+	}
+	
+	public static void main(String[] args) {
+		int num = 5;		
+		System.out.print(num + "! = ");
+		System.out.println(factorial(num));
+	}
+
+}
