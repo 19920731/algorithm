f596fb2ed050507e9524a17e10603ae6629afbaa
choi <younggeunn@naver.com>
2020-09-13 20:32:38 +0900

    SelectionSort

---
+++ src/sort/SelectionSort.java
@@ -0,0 +1,32 @@
+package sort;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		int[] arr = {2, 4, 3, 5, 1};
+		int min;
+		int minIdx;
+		int tmp;
+		
+		for (int i = 0; i < arr.length; i++) {
+			min = arr[i];
+			minIdx = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] < min) {
+					min = arr[j];
+					minIdx = j;
+				}
+			}
+			
+			tmp = arr[i];
+			arr[i] = arr[minIdx];
+			arr[minIdx] = tmp;
+			
+		}
+		
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+	}
+
+}
