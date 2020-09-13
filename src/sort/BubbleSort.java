fe470fe618cf5cd8510abb35457595f0d272b9bf
choi <younggeunn@naver.com>
2020-09-13 18:47:10 +0900

    Bubble Sort

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,24 @@
+package sort;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		int[] arr = {6, 4, 3, 7, 1, 9, 8};
+		int tmp;
+		
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					tmp = arr[j + 1];
+					arr[j + 1] = arr[j];
+					arr[j] = tmp;
+				}
+			}
+		}
+		
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+	}
+
+}
