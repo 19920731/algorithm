6cf75b81abd3ab4acf0457ac60b5604da0e8712f
choi <younggeunn@naver.com>
2020-10-31 19:26:56 +0900

    유클리드 호제법(재귀)

---
+++ src/recursion/Euclid.java
@@ -0,0 +1,18 @@
+package recursion;
+
+public class Euclid {
+	
+	public static int gcd(int p, int q) {
+		if (q == 0) {
+			return p;
+		}
+		return gcd(q, p % q);
+	 }
+	public static void main(String[] args) {
+		int num1 = 18;
+		int num2 = 27;
+		
+		System.out.println(gcd(num1, num2));
+	}
+
+}
