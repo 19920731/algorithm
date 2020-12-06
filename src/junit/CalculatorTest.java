62af2f9f650009d0da3690a03e8191197451b615
choi <younggeunn@naver.com>
2020-12-06 19:38:48 +0900

    JUnit Sample

---
+++ src/junit/CalculatorTest.java
@@ -0,0 +1,37 @@
+package junit;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	@Test
+	public void testSum() {
+		Calculator calculator = new Calculator();
+		 
+        int resultSum = calculator.sum(5, 5);
+        System.out.println("resultSum : " + resultSum);        
+	}
+	
+	@Test
+	public void testSub() {
+		Calculator calculator = new Calculator();
+		
+        int resultSub = calculator.sub(5, 5);
+        System.out.println("resultSub : " + resultSub);        
+	}
+	
+	@Test
+	public void testMul() {
+		Calculator calculator = new Calculator();
+		
+        int resultMul = calculator.mul(5, 5);
+        System.out.println("resultMul : " + resultMul);        
+	}
+	
+	@Test
+	public void testDiv() {
+		Calculator calculator = new Calculator();
+		
+        int resultDiv = calculator.div(5, 5);
+        System.out.println("resultDiv : " + resultDiv);        
+	}
+}
