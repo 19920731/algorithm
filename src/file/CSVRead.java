8d8d3a40cdb65b8970b6b10742906a6205ddf9ca
choi <younggeunn@naver.com>
2020-12-06 14:04:29 +0900

    정렬기능 추가

--- src/file/CSVRead.java
+++ src/file/CSVRead.java
@@ -5,8 +5,17 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 public class CSVRead {
+	
+	public static void print(ArrayList<HashMap<String, String>> memberList, String sortType) {
+		System.out.println("=== 회원정보 (" + sortType + ") ===");
+		for (int i = 0; i < memberList.size(); i++) {
+			System.out.println(memberList.get(i));
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 
@@ -33,7 +42,31 @@ public class CSVRead {
 				memberList.add(map);
 			}
 			
-			System.out.println(memberList);
+			print(memberList, "정렬없음");
+			
+			// 이름순 정렬
+			Collections.sort(memberList, new Comparator<HashMap<String, String>>() {
+				@Override
+				public int compare(HashMap<String, String> map1, HashMap<String, String> map2) {
+					String name = (String) map1.get("memberName");
+					String name2 = (String) map2.get("memberName");
+					return name.compareTo(name2);
+				}
+			});
+			
+			print(memberList, "이름순");
+			
+			// 사원번호순 정렬
+			Collections.sort(memberList, new Comparator<HashMap<String, String>>() {
+				@Override
+				public int compare(HashMap<String, String> map1, HashMap<String, String> map2) {
+					String memberId1 = (String) map1.get("memberId");
+					String memberId2 = (String) map2.get("memberId");
+					return memberId1.compareTo(memberId2);
+				}
+			});
+			
+			print(memberList, "사원번호순");
 			
 		} catch (Exception e) {
 			System.out.println(e.toString());
