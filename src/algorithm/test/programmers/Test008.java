a5cb9aa669b72ecc820e54866923764f9de62b1b
choi <younggeunn@naver.com>
2020-12-15 11:52:06 +0900

    Create Test008.java

---
+++ src/algorithm/test/programmers/Test008.java
@@ -0,0 +1,26 @@
+package algorithm.test.programmers;
+
+// 프로그래머스 > 연습문제 > 가운데 글자 가져오기
+public class Test008 {
+
+	public static String solution(String s) {
+        StringBuffer sb = new StringBuffer(2);
+		String answer = "";
+		if (s.length() % 2 == 0) {
+        	sb.append(s.charAt(s.length() / 2 - 1));
+        	sb.append(s.charAt(s.length() / 2));
+        } else {
+        	sb.append(s.charAt(s.length() / 2));
+        }
+        
+        answer = sb.toString();
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		// String a = "abced";
+		String a = "qwer";
+		System.out.println(solution(a));
+	}
+
+}
