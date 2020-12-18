d9c7168b5b1f453f6d38ee7286a16d29172e5ac2
choi <younggeunn@naver.com>
2020-12-18 10:48:38 +0900

    Create LambdaSample001.java

---
+++ src/lambda/LambdaSample001.java
@@ -0,0 +1,35 @@
+package lambda;
+
+class RunTest implements Runnable {
+	@Override
+	public void run() {
+		System.out.println("기존 방식 호출!");
+	}	
+}
+
+public class LambdaSample001 {
+
+	public static void main(String[] args) {
+		// 기존 방식
+		RunTest r1 = new RunTest();
+		r1.run();
+		
+
+		// 익명 클래스
+		Runnable r2 = new Runnable() {			
+			@Override
+			public void run() {
+				System.out.println("익명 클래스 호출!");
+			}
+		};		
+		r2.run();
+
+		
+		// 람다
+		Runnable r3 = () -> {
+			System.out.println("람다 방식의 호출!");
+		};		
+		r3.run();
+	}
+
+}
